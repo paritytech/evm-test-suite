269cd9cdeedc27f30373ba682e49fcb0859920d7
bee344 <alberto.penayo@parity.io>
2024-11-04 10:39:24 -0300

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,49 +1,45 @@
 #!/bin/sh
 
-case "$1" in
+chain=$1
+url=$2
+tests=$3
+
+case "$chain" in
   --acala)
     export NETWORK_URL="http://localhost:8545"
-    export TEST_CASE="$2"
     ;;
   --ethereum)
     export NETWORK_URL="https://ethereum-rpc.publicnode.com"
-    export TEST_CASE="$2"
     ;;
   --moonbeam)
     export NETWORK_URL="https://moonbeam.public.blastapi.io"
-    export TEST_CASE="$2"
     ;;
   --astar)
     export NETWORK_URL="https://rpc.astar.network"
-    export TEST_CASE="$2"
     ;;
   --polygon)
     export NETWORK_URL="https://polygon-amoy-bor-rpc.publicnode.com"
-    export TEST_CASE="$2"
     ;;
   --westend)
     export NETWORK_URL="https://westend-asset-hub-eth-rpc.polkadot.io/"
-    export TEST_CASE="$2"
     ;;
   --endpoint | -e)
     export NETWORK_URL="$2"
-    export TEST_CASE="$3"
     ;;
   *)
     export NETWORK_URL="https://ethereum-rpc.publicnode.com"
-    export TEST_CASE="$2"
     ;;
 esac
 
-if [ "$1" = '--acala' ]; then
+if [ "$chain" = '--acala' ]; then
   npx @acala-network/eth-rpc-adapter -e wss://acala-rpc-2.aca-api.network/ws &
   sleep 10
 fi
 
-if [ "$TEST_CASE" = '--matter-labs' ]; then
+if [ "$tests" = '--matter-labs' ]; then
   cd ./matter-labs-tests/ &&
   npx hardhat test ./test/MatterLabsTests.ts
-elif [ "$TEST_CASE" = '--smart-contracts' ]; then
+elif [ "$tests" = '--smart-contracts' ]; then
   npx hardhat compile &&
   npx hardhat test &&
   cd ./v3-core/ &&
