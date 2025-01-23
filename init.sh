0980b821bc09a05515bb1cd4152d64f3899b92a4
bee344 <alberto.penayo@parity.io>
2025-01-22 22:55:56 -0300

    removed old configs

--- init.sh
+++ init.sh
@@ -42,192 +42,48 @@ run_matter_labs_tests() {
   parse_hardhat_test_results "../test-logs/matter-labs-tests.log"
 }
 
-run_smart_contracts_tests() {
-  if ! command -v forge >/dev/null 2>&1; then
-    echo "Setting Up Foundry..."
-    curl -L https://foundry.paradigm.xyz | bash
-
-    case "$SHELL" in
-      */bash)
-        source ~/.bashrc
-        ;;
-      */zsh)
-        source ~/.zshenv
-        ;;
-      *)
-        echo "Unknown shell: $SHELL"
-        ;;
-    esac
-
-    foundryup
-  else
-    echo "Foundry is already installed. Skipping installation."
-  fi
-
-  yarn install && echo "Running Smart Contract V3 Tests"
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ./config/general/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ./config/general/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  npx hardhat test | tee "$LOG_DIR/smart-contract-v3-tests.log"
-  parse_hardhat_test_results "$LOG_DIR/smart-contract-v3-tests.log"
-
-  cd ./v3-core/ && yarn install
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ../config/v3-core/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ../config/v3-core/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  yarn test | tee "../$LOG_DIR/v3-core-tests.log"
-  parse_hardhat_test_results "../$LOG_DIR/v3-core-tests.log"
-
-  echo "Running Smart Contract Periphery Tests"
-  cd ../v3-periphery/ && yarn install
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ../config/v3-periphery/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ../config/v3-periphery/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  yarn test | tee "../$LOG_DIR/v3-periphery-tests.log"
-  parse_hardhat_test_results "../$LOG_DIR/v3-periphery-tests.log"
-
-  case "$USE_REVIVE" in
-    false)
-      echo "Running Smart Contract CCTP Tests" &&
-      cd ../evm-cctp-contracts/ &&
-      git submodule update --init --recursive &&
-      yarn install &&
-      forge test --rpc-url $NETWORK_URL --no-match-test "testReceiveMessage_succeedsWithNonzeroDestinationCaller|testReplaceMessage_succeeds|testReplaceMessage_succeedsButFailsToReserveNonceInReceiveMessage|testSetMaxMessageBodySize|testDepositForBurnWithCaller_returnsNonzeroNonce|testDepositForBurnWithCaller_succeeds|testHandleReceiveMessage_succeedsForMint" | tee "../$LOG_DIR/evm-cctp-tests.log"
-      parse_forge_test_results "../$LOG_DIR/evm-cctp-tests.log"
-      ;;
-    *)
-      ;;
-  esac
-
-  echo "Smart Contracts Test Run Complete"
+run_open_zeppelin_tests() {
+    echo "Running Open Zeppelin EVM Tests" &&
+    cd ./openzeppelin-contracts &&
+    npm i &&
+    git submodule update --init --recursive &&
+    TEST_LOG="../$LOG_DIR/open-zeppelin-tests.log" &&
+  npx hardhat test --config ./${HARDHAT_CONFIG_NAME} | tee "$TEST_LOG"
+  parse_hardhat_test_results "../test-logs/open-zeppelin-tests.log" 
 }
 
-
-run_matter_labs_and_then_smart_contracts_tests() {
+run_matter_labs_and_then_oz_tests() {
   echo "Running Matter Labs EVM Tests" &&
   cd ./matter-labs-tests/contracts &&
-  yarn install &&
+  npm i &&
+  cd .. &&
   git submodule update --init --recursive &&
 
   case "$USE_REVIVE" in
     true)
-      npx hardhat compile --config ../../config/matter-labs/revive.config.ts
+      npx hardhat compile --config hardhat.config
       ;;
     *)
-      npx hardhat compile --config ../../config/matter-labs/${HARDHAT_CONFIG_NAME}
+      npx hardhat compile --config hardhat.evm.config --network ${chain}
       ;;
   esac
 
-  npx hardhat test ../test/MatterLabsTests.ts | tee "../../$LOG_DIR/matter-labs-tests.log"
-  parse_hardhat_test_results "../../$LOG_DIR/matter-labs-tests.log"
+  npx hardhat test | tee "../$LOG_DIR/matter-labs-tests.log"
+  parse_hardhat_test_results "../$LOG_DIR/matter-labs-tests.log"
 
-  cd ../..
+  cd ..
 
-  if ! command -v forge >/dev/null 2>&1; then
-    echo "Setting Up Foundry..."
-    curl -L https://foundry.paradigm.xyz | bash
+  echo "Running Open Zeppelin Tests"
 
-    case "$SHELL" in
-      */bash)
-        source ~/.bashrc
-        ;;
-      */zsh)
-        source ~/.zshenv
-        ;;
-      *)
-        echo "Unknown shell: $SHELL"
-        ;;
-    esac
+  cd ./openzeppelin-contracts &&
 
-    foundryup
-  else
-    echo "Foundry is already installed. Skipping installation."
-  fi
+  npx hardhat compile --config ${HARDHAT_CONFIG_NAME}.js
 
-  echo "Running Smart Contract Tests"
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ./config/general/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ./config/general/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  npx hardhat test | tee "$LOG_DIR/smart-contract-v3-tests.log"
-  parse_hardhat_test_results "$LOG_DIR/smart-contract-v3-tests.log"
-
-  cd ./v3-core/ &&
-  yarn install
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ../config/v3-core/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ../config/v3-core/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  yarn test | tee "../$LOG_DIR/v3-core-tests.log"
-  parse_hardhat_test_results "../$LOG_DIR/v3-core-tests.log"
-
-  echo "Running Smart Contract Periphery Tests"
-
-  cd ../v3-periphery/ &&
-  yarn install
-
-  case "$USE_REVIVE" in
-    true)
-      npx hardhat compile --config ../config/v3-periphery/revive.config.ts
-      ;;
-    *)
-      npx hardhat compile --config ../config/v3-periphery/${HARDHAT_CONFIG_NAME}
-      ;;
-  esac
-
-  yarn test | tee "../$LOG_DIR/v3-periphery-tests.log"
-  parse_hardhat_test_results "../$LOG_DIR/v3-periphery-tests.log"
-
-  case "$USE_REVIVE" in
-    false)
-      echo "Running Smart Contract CCTP Tests" &&
-      cd ../evm-cctp-contracts/ &&
-      git submodule update --init --recursive &&
-      yarn install &&
-      forge build &&
-      forge test --rpc-url $NETWORK_URL --no-match-test "testReceiveMessage_succeedsWithNonzeroDestinationCaller|testReplaceMessage_succeeds|testReplaceMessage_succeedsButFailsToReserveNonceInReceiveMessage|testSetMaxMessageBodySize|testDepositForBurnWithCaller_returnsNonzeroNonce|testDepositForBurnWithCaller_succeeds|testHandleReceiveMessage_succeedsForMint" | tee "../$LOG_DIR/evm-cctp-tests.log"
-      parse_forge_test_results "../$LOG_DIR/evm-cctp-tests.log"
-      ;;
-    *)
-      ;;
-  esac
+  npx hardhat test | tee "../$LOG_DIR/open-zeppelin-tests.log"
+  parse_hardhat_test_results "../$LOG_DIR/open-zeppelin-tests.log"
 
   echo "Test Run Complete"
 }
-
 
 parse_hardhat_test_results() {
   log_file=$1
@@ -252,88 +108,72 @@ parse_hardhat_test_results() {
   echo "Total: $total | Passed: $passed | Failed: $failed"
 }
 
-parse_forge_test_results() {
-  log_file=$1
-  passed=$(grep -o 'Passed' "$log_file" | wc -l)
-  failed=$(grep -o 'Failed' "$log_file" | wc -l)
-
-  if [ -z "$passed" ]; then
-    passed=0
-  fi
-
-  if [ -z "$failed" ]; then
-    failed=0
-  fi
-
-  total=$((passed + failed))
-
-  total_passed=$((total_passed + passed))
-  total_failed=$((total_failed + failed))
-  total_tests=$((total_tests + total))
-
-  echo "Foundry Test Summary from $log_file:"
-  echo "Total: $total | Passed: $passed | Failed: $failed"
-}
-
 case "$chain" in
 --acala)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="http://localhost:8545"
   export CHAIN_ID=787
   export NETWORK_NAME="acala"
   ;;
 --ethereum)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="http://localhost:8545"
   export CHAIN_ID=1
+  export NETWORK_NAME="ethereum"
   ;;
 --moonbeam)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="https://moonbeam.public.blastapi.io"
   export CHAIN_ID=1284
+  export NETWORK_NAME="moonbeam"
   ;;
 --astar)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="http://localhost:8000"
   export CHAIN_ID=592
+  export NETWORK_NAME="astar"
   ;;
 --polygon)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="https://polygon-mainnet.infura.io/v3/${PRIVATE_KEY}"
   export CHAIN_ID=157
+  export NETWORK_NAME="polygon"
   ;;
 --arbitrum)
-  export HARDHAT_CONFIG_NAME="hardhat.evm.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.evm.config"
   export USE_REVIVE="false"
   export NETWORK_URL="https://arbitrum-mainnet.infura.io/v3/${PRIVATE_KEY}"
   export CHAIN_ID=42161
+  export NETWORK_NAME="arbitrum"
   ;;
 --kitchensink)
-  export HARDHAT_CONFIG_NAME="hardhat.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.config"
   export USE_REVIVE="true"
   export NETWORK_URL="http://localhost:8545"
   ;;
 --westend)
-  export HARDHAT_CONFIG_NAME="hardhat.config.ts"
+  export HARDHAT_CONFIG_NAME="hardhat.config"
   export USE_REVIVE="true"
   export NETWORK_URL="http://localhost:8545"
+  export USE_FORKING="true"
   ;;
 --endpoint | -e)
   if [ "${USER_REVIVE}" = "true" ]; then
-    export HARDHAT_CONFIG_NAME="revive.config.ts"
+    export HARDHAT_CONFIG_NAME="revive.config"
   else
-    export HARDHAT_CONFIG_NAME="hardhat.config.ts"
+    export HARDHAT_CONFIG_NAME="hardhat.config"
   fi
   ;;
 *)
-  export HARDHAT_CONFIG_NAME="ethereum.config.ts"
+  export HARDHAT_CONFIG_NAME="ethereum.config"
   export USE_REVIVE="false"
   export NETWORK_URL="https://ethereum-rpc.publicnode.com"
+  export CHAIN_ID=1
   ;;
 esac
 
@@ -342,7 +182,7 @@ echo $chain
 case "$chain" in
   --ethereum)
     echo "Cleaning up"
-    rm -rf ./output-logs/*
+    rm -rf ./output-logs/geth-output.log
     echo "Starting Geth Ethereum Node"
     ./networks/ethereum/build/bin/${NETWORK_DIR}/geth --datadir ./networks/ethereum/node1 init ./networks/ethereum/genesis.json && \
       ./networks/ethereum/build/bin/${NETWORK_DIR}/geth --datadir ./networks/ethereum/node1 --syncmode "full" \
@@ -358,13 +198,13 @@ case "$chain" in
 
   --acala)
     echo "Cleaning up"
-    rm -rf ./output-logs/*
+    rm -rf ./output-logs/acala-chopsticks-output.log
     echo "Starting Chopsticks instance"
     yarn add @acala-network/chopsticks@latest &&
-      npx @acala-network/chopsticks@latest --endpoint=wss://acala-rpc-2.aca-api.network/ws > ./output-logs/chopsticks-output.log 2>&1 &
+      npx @acala-network/chopsticks@latest --endpoint=wss://acala-rpc-2.aca-api.network/ws > ./output-logs/acala-chopsticks-output.log 2>&1 &
     echo "Waiting for the Chopsticks to start on ws://[::]:8000..."
 
-    while ! grep -q "app: " ./output-logs/chopsticks-output.log; do
+    while ! grep -q "app: " ./output-logs/acala-chopsticks-output.log; do
       sleep 1
     done
 
@@ -384,13 +224,13 @@ case "$chain" in
 
   --astar)
     echo "Cleaning up"
-    rm -rf ./output-logs/*
+    rm -rf ./output-logs/astar-chopsticks-output.log
     echo "Starting Chopsticks instance"
     yarn add @acala-network/chopsticks@latest &&
-      npx @acala-network/chopsticks@latest --endpoint=wss://rpc.astar.network > ./output-logs/chopsticks-output.log 2>&1 &
+      npx @acala-network/chopsticks@latest --endpoint=wss://rpc.astar.network > ./output-logs/astar-chopsticks-output.log 2>&1 &
     echo "Waiting for the Chopsticks to start on ws://[::]:8000..."
 
-    while ! grep -q "app: " ./output-logs/chopsticks-output.log; do
+    while ! grep -q "app: " ./output-logs/astar-chopsticks-output.log; do
       sleep 1
     done
 
@@ -398,48 +238,14 @@ case "$chain" in
     ;;
 
   --kitchensink)
-    echo "Cleaning up"
-    rm -rf ./output-logs/*
-    echo "Starting Kitchensink Node"
-    RUST_LOG="error,evm=debug,sc_rpc_server=info,runtime::revive=debug" ./networks/westend/${NETWORK_DIR}/substrate-node --dev > ./output-logs/node-output.log 2>&1 &
-
-
-    while ! grep -q "Running JSON-RPC server:" ./output-logs/node-output.log; do
-      sleep 1
-    done
-    echo "Kitchensink node running"
-
-    echo "Starting Eth RPC Adapter"
-    RUST_LOG="info,eth-rpc=debug" ./networks/westend/${NETWORK_DIR}/eth-rpc --node-rpc-url ws://127.0.0.1:9944 --dev > ./output-logs/eth-rpc-output.log 2>&1 &
-    
-    while ! grep -q "Running JSON-RPC server:" ./output-logs/eth-rpc-output.log; do
-      sleep 1
-    done
-
-    echo "Eth RPC Adapter running"
+    echo "Runninc Kitchensink"
     ;;
 
   --westend)
-    echo "Cleaning up"
-    rm -rf ./output-logs/*
-    echo "Starting Chopsticks instance"
-    yarn add @acala-network/chopsticks@latest &&
-      npx @acala-network/chopsticks@latest --endpoint=wss://asset-hub-westend-rpc.dwellir.com > ./output-logs/chopsticks-output.log 2>&1 &
-    echo "Waiting for the Chopsticks to start on ws://[::]:8000..."
-
-    while ! grep -q "app: " ./output-logs/chopsticks-output.log; do
-      sleep 1
-    done
-
-    echo "Chopsticks instance now running on ws://[::]:8000."
-    echo "Starting Eth RPC Adapter"
-    RUST_LOG="info,eth-rpc=debug" ./networks/westend/${NETWORK_DIR}/eth-rpc --node-rpc-url ws://127.0.0.1:8000 --dev > ./output-logs/revive-rpc-output.log 2>&1 &
-    sleep 15
+    echo "Forking Asset Hub Westend"
     ;;
 
   *)
-    echo "Cleaning up"
-    rm -rf ./output-logs/*
     echo "Unknown chain: $chain"
     ;;
 esac
@@ -453,14 +259,14 @@ case "$chain" in
         kill -9 $(lsof -t -i:30304)
         echo "Geth Ethereum Node Stopped"
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
+      --open-zeppelin)
+        run_open_zeppelin_tests
         sleep 1
         kill -9 $(lsof -t -i:30304)
         echo "Geth Ethereum Node Stopped"
         ;;
       *)
-        run_matter_labs_and_then_smart_contracts_tests
+        run_matter_labs_and_then_oz_tests
         sleep 1
         kill -9 $(lsof -t -i:30304)
         echo "Geth Ethereum Node Stopped"
@@ -478,8 +284,8 @@ case "$chain" in
         kill -9 $(lsof -t -i:8000)
         echo "Chopsticks Instance Stopped"
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
+      --open-zeppelin)
+        run_open_zeppelin_tests
         sleep 1
         kill -9 $(lsof -t -i:8545)
         echo "Eth RPC Adapter Instance Stopped"
@@ -487,8 +293,7 @@ case "$chain" in
         echo "Chopsticks Instance Stopped"
         ;;
       *)
-      echo "RunninT"
-        run_matter_labs_and_then_smart_contracts_tests
+        run_matter_labs_and_then_oz_tests
         sleep 1
         kill -9 $(lsof -t -i:8545)
         echo "Eth RPC Adapter Instance Stopped"
@@ -506,14 +311,14 @@ case "$chain" in
         kill -9 $(lsof -t -i:800)
         echo "Chopsticks Instance Stopped"
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
+      --open-zeppelin)
+        run_open_zeppelin_tests
         sleep 1
         kill -9 $(lsof -t -i:8000)
         echo "Chopsticks Instance Stopped"
         ;;
       *)
-        run_matter_labs_and_then_smart_contracts_tests
+        run_matter_labs_and_then_oz_tests
         sleep 1
         kill -9 $(lsof -t -i:8000)
         echo "Chopsticks Instance Stopped"
@@ -525,27 +330,12 @@ case "$chain" in
     case "$tests" in
       --matter-labs)
         run_matter_labs_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:9944)
-        echo "Kitchensink Instance Stopped"
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:9944)
-        echo "Kitchensink Instance Stopped"
+      --open-zeppelin)
+        run_open_zeppelin_tests
         ;;
       *)
-        run_matter_labs_and_then_smart_contracts_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:9944)
-        echo "Kitchensink Instance Stopped"
+        run_matter_labs_and_then_oz_tests
         ;;
     esac
     ;;
@@ -554,27 +344,12 @@ case "$chain" in
     case "$tests" in
       --matter-labs)
         run_matter_labs_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:8000)
-        echo "Chopsticks Instance Stopped"
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:8000)
-        echo "Chopsticks Instance Stopped"
+      --open-zeppelin)
+        run_open_zeppelin_tests
         ;;
       *)
-        run_matter_labs_and_then_smart_contracts_tests
-        sleep 1
-        kill -9 $(lsof -t -i:8545)
-        echo "Eth RPC Adapter Instance Stopped"
-        kill -9 $(lsof -t -i:8000)
-        echo "Chopsticks Instance Stopped"
+        run_matter_labs_and_then_oz_tests
         ;;
     esac
     ;;
@@ -584,11 +359,11 @@ case "$chain" in
       --matter-labs)
         run_matter_labs_tests
         ;;
-      --smart-contracts)
-        run_smart_contracts_tests
+      --open-zeppelin)
+        run_open_zeppelin_tests
         ;;
       *)
-        run_matter_labs_and_then_smart_contracts_tests
+        run_matter_labs_and_then_oz_tests
         ;;
     esac
     ;;
