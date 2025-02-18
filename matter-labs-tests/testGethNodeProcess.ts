7e3e10a048c06180ad7f76c98eb116c66380d01b
marshacb <cameron.marshall12@gmail.com>
2025-02-18 12:33:51 -0500

    test child processes

---
+++ matter-labs-tests/testGethNodeProcess.ts
@@ -0,0 +1,64 @@
+import { spawn } from "child_process";
+import * as http from 'http';
+
+const ethereumNode = '/Users/cameron-parity/workspace/evm-test-suite/networks/ethereum/build/bin/macOS/geth';
+const gethArgs = [
+	'--syncmode', 'snap',
+	'--http',
+	'--http.port', '8546',
+	'--http.api', 'eth,net,web3',
+];
+
+const ethereumNodeProcess = spawn(ethereumNode, gethArgs);
+
+// stdout
+ethereumNodeProcess.stdout.on('data', (data) => {
+	const output = data.toString();
+	console.log(`stdout: ${output}`);
+
+	// Check node ready message
+	if (output.includes('HTTP endpoint opened')) {
+		console.log('Ethereum node is ready!')
+	}
+});
+
+ethereumNodeProcess.stderr.on('data', (data) => {
+	console.log(`stderr: ${data}`);
+});
+
+ethereumNodeProcess.on('close', (code) => {
+	console.log(`Ethereum node process exited with code ${code}`);
+});
+
+const checkEthereumNodeReady = () => {
+	const options = {
+		hostname: '127.0.0.1',
+		port: 8546,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	const req = http.request(options, (res) => {
+		if (res.statusCode === 200) {
+			console.log("Ethereum node is up and running!")
+		} else {
+			console.log(`Node not ready, status code: ${res.statusCode}`);
+		}
+	});
+
+	req.on('error', (e) => {
+		console.error(`Error checking node status: ${e.message}`);
+	});
+
+	req.write(
+		JSON.stringify({
+			jsonrpc: '2.0',
+			method: 'net_version',
+			params: [],
+			id: 1,
+		});
+	);
+}
+
