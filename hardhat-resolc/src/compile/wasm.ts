d3a1b45b6a55efa20895b5ed64522d70b887809d
bee344 <alberto.penayo@parity.io>
2025-02-19 17:18:45 -0300

    last commit

--- hardhat-resolc/src/compile/wasm.ts
+++ hardhat-resolc/src/compile/wasm.ts
@@ -2,13 +2,17 @@ import { SolcOutput } from "../types";
 import { exec as execCb } from 'child_process';
 import { promisify } from 'util';
 import { compile, resolveInputs } from '@parity/revive';
+import { readFileSync, writeFileSync } from "fs";
 
 const exec = promisify(execCb);
 
 export async function compileWithWasm(sources: any): Promise<SolcOutput> {
-    const updatedSources = resolveInputs(sources)
 
-    const out = await compile(updatedSources);
+    // writeFileSync('/home/bee344/Documentos/parity/evm-test-suite/input.log', JSON.stringify(sources));
+
+    // console.log(JSON.parse(readFileSync('/home/bee344/Documentos/parity/evm-test-suite/input.log', 'utf-8')))
+
+    const out = await compile(sources["sources"]);
 
     return out;
 }
