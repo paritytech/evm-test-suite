d20230d0a70ad1159ee75b4c56a47e85173f19e5
Robert van Eerdewijk <robert@Roberts-MacBook-Pro.local>
2025-10-19 11:49:59 +0200

    fix ci

--- src/methods.test.ts
+++ src/methods.test.ts
@@ -29,7 +29,7 @@ Deno.test('eth_accounts', opts, async () => {
     const addresses = await env.debugClient.request({
         method: 'eth_accounts',
     })
-    expect(addresses).toBeGreaterThan(0)
+    expect(addresses.length).toBeGreaterThanOrEqual(1)
 })
 
 Deno.test('eth_blockNumber', opts, async () => {
